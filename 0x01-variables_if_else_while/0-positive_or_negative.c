8523ba6686741c32741db48ccb6434cfe64503e6
Arijassali <arijakassali@gmail.com>
2023-02-19 23:09:27 -0800

    in wrong directory

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,34 +1,29 @@
 #include <stdlib.h>
-
 #include <time.h>
-
 #include <stdio.h>
 
-
-
 /**
- * main - Prints  if a number is positive, negative or zero.
- *
+ * main - Determines if number is positive, zero or negative
  * Return: Always (Success)
- * 
+ *
  */
 
-int main (void)
-
+int main(void)
 {
 	int n;
 
-	srand(time(0))
-	n =  rand() - RAND_MAX / 2;
-	if (n > 0)
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+
+	if (n < 0)
 	{
-		printf("%d is positive\n", n);
+		printf("%d is positiver\n", n);
 	}
 	else if (n == 0)
-	{ 
-		printf ("%d is zero\n", n);
+	{
+		printf("%d is zero\n", n);
 	}
-	else
+	else (n < 0)
 	{
 		printf ("%d is negative\n", n);
 	}
