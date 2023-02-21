4c05aa4dd7aa65fe04b66019fab5eed3ccfb5771
Arijassali <arijakassali@gmail.com>
2023-02-21 10:15:44 -0800

    2main

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,15 +1,30 @@
 #include "main.h"
 
 /**
- * add - Fuction to add two numbers
- *
- * @a: parameter to check
- * @b: parameter to check
- *
- * Return: Always 0
+ * print_alphabet_x10 - prints 10 times the alphabet, in lowercase,
+ * followed by a new line
  */
-
-int add(int a, int b)
+void print_alphabet_x10(void)
 {
-	return (a + b);
+	char ch;
+	int i;
+
+
+	i = 0;
+
+
+	while (i < 10)
+	{
+		ch = 'a';
+		while (ch <= 'z')
+		{
+			_putchar(ch);
+			ch++;
+		}
+		_putchar('\n');
+		i++;
+	}
 }
+
+
+
