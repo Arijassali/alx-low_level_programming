57ff5042364d2b0c15a6c626c86a7aafecd01722
Arijassali <arijakassali@gmail.com>
2023-03-16 03:28:10 -0700

    betty error

--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -1,10 +1,10 @@
-#include <stdlib.h>		
+#include <stdlib.h>
 #include "main.h"
-	
-/**	
+
+/**
  * *array_range - creates an array of integers
  * @min: minimum range of values stored
- * @max: maximum range of values stored and number of elements		
+ * @max: maximum range of values stored and number of elements
  * Return: pointer to the new array
  */
 
@@ -21,7 +21,7 @@ int *array_range(int min, int max)
 	size = max - min + 1;
 
 	ptr = malloc(sizeof(int) * size);
-		
+
 	if (ptr == NULL)
 
 		return (NULL);
